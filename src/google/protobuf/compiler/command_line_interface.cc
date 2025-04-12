c99937fddff30ba2e265c97a11c1e48797cdeadf
Pierce Brooks <piercebrooks@rocketmail.com>
2025-04-12 00:28:50 -0700

    fix protoc cli invokation cases where an arg is an absolute path and the binary was built inside msys but is being called outside from the native windows cmd shell

--- src/google/protobuf/compiler/command_line_interface.cc
+++ src/google/protobuf/compiler/command_line_interface.cc
@@ -116,7 +116,7 @@ static const char* kDefaultDirectDependenciesViolationMsg =
 // with a drive letter.  Example:  "C:\foo".  TODO(kenton):  Share this with
 // copy in importer.cc?
 static bool IsWindowsAbsolutePath(const std::string& text) {
-#if defined(_WIN32) || defined(__CYGWIN__)
+#if defined(_WIN32) || defined(__CYGWIN__) || defined(__MSYS__) || defined(__MSYS2__)
   return text.size() >= 3 && text[1] == ':' && isalpha(text[0]) &&
          (text[2] == '/' || text[2] == '\\') && text.find_last_of(':') == 1;
 #else
@@ -766,7 +766,7 @@ CommandLineInterface::MemoryOutputStream::~MemoryOutputStream() {
 
 // ===================================================================
 
-#if defined(_WIN32) && !defined(__CYGWIN__)
+#if defined(_WIN32) && !defined(__CYGWIN__) && !defined(__MSYS__) && !defined(__MSYS2__)
 const char* const CommandLineInterface::kPathSeparator = ";";
 #else
 const char* const CommandLineInterface::kPathSeparator = ":";
@@ -1463,16 +1463,30 @@ CommandLineInterface::InterpretArgument(const std::string& name,
 #endif  // _WIN32
 
   } else if (name == "-I" || name == "--proto_path") {
+    std::string value_split = value;
+#if defined(__CYGWIN__) || defined(__MSYS_) || defined(__MSYS2__)
+    if (IsWindowsAbsolutePath(value_split)) {
+      value_split[1] = value_split[0];
+      value_split[0] = '/';
+    }
+#endif
+
     // Java's -classpath (and some other languages) delimits path components
     // with colons.  Let's accept that syntax too just to make things more
     // intuitive.
     std::vector<std::string> parts = Split(
-        value, CommandLineInterface::kPathSeparator,
+        value_split, CommandLineInterface::kPathSeparator,
         true);
 
     for (int i = 0; i < parts.size(); i++) {
       std::string virtual_path;
       std::string disk_path;
+
+#if defined(__CYGWIN__) || defined(__MSYS_) || defined(__MSYS2__)
+      if (parts[i] == value_split) {
+        parts[i] = value;
+      }
+#endif
 
       std::string::size_type equals_pos = parts[i].find_first_of('=');
       if (equals_pos == std::string::npos) {
